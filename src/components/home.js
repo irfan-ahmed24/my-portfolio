c5aedc3b47b28eb614e781b02c611ba4f7f576db
irfan-ahmed24 <irfanahmed99989@gmail.com>
2024-11-01 23:27:36 +0600

    home

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,17 @@
 import React from "react";
-
 function Home() {
   return (
     <div className="home" id="home">
       <h1>Home</h1>
       <div className="Home-page">
         <div className="photo">
-          <img src="./picture/irfan.jpg" />
+          <img src="./picture/irfan.jpg" alt="" />
         </div>
         <div className="intro">
           <span>
             <h3>Welcome to my portfolio</h3>
             <h2>Hello, I'm Irfan Ahmed</h2>
-            <h3>Web Developer</h3>
+            <h3>Website Developer</h3>
           </span>
         </div>
       </div>
