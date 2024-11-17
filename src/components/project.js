a16ceb7ebc6b2d24c11f7adfe5e80563b586bfc7
irfan-ahmed24 <irfanahmed99989@gmail.com>
2024-11-17 21:34:47 +0600

    for test

--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,7 @@ export default class Project extends Component {
   render() {
     return (
       <div>
-        <h1>this is project section</h1>
+        <h3>Project section comming soon</h3>
       </div>
     );
   }
